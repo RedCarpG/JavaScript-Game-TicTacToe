1bb1c8ed2690474213800bd9fe3895682cb363fc
RedCarpG <redcarpg@gmail.com>
2022-01-21 18:58:32 +0100

    update: complete EndPage.js with CSSTransition

--- src/components/EndPage.js
+++ src/components/EndPage.js
@@ -1,61 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import FloatButton from "./FloatButton";
-import Mark from "./Mark";
-import { STATUS } from "../constant/common.js";
+import { MARK, STATUS } from "../constant/common.js";
 import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 import { useTurn, useGameState, useTurnUpdate, useGameStateUpdate } from "./GameContext";
-export default function EndPage( {} ) {
+import { CSSTransition } from "react-transition-group";
+
+function EndPageMessage({ text }) {
+    return (
+        <div className="tw-flex tw-flex-row tw-justify-center tw-items-center tw-text-8xl tw-gap-5"> 
+            {text}
+        </div>
+    )
+}
+
+function EndPageIcons() {
+    return (
+        <div className="tw-flex tw-flex-row tw-gap-5 tw-text-4xl">
+            <a href="https://github.com/RedCarpG" target="_blank" rel="noreferrer">
+                <FaGithubSquare />
+            </a>
+            <a href="https://www.linkedin.com/in/peng-gao-fr/" target="_blank" rel="noreferrer">
+                <FaLinkedin />
+            </a>
+        </div>
+    )
+}
+
+export default function EndPage() {
     const turn = useTurn()
     const gameState = useGameState()
     const toggleTurn = useTurnUpdate()
     const toggleGameState = useGameStateUpdate()
 
-    const [visible, setVisibility] = useState(false)
+    const [message, setMessage] = useState(<></>)
 
     useEffect(() => {
-        if (gameState===STATUS.INGAME) {
-            setVisibility(false)
-        } else {
-            setVisibility(true)
-        }
-    }, [gameState])
+        if (gameState === STATUS.WIN) setMessage(<> <embed className={"mark-img spin"} src={turn===MARK.X?"./img/x.svg":"./img/o.svg"} type="image/svg+xml" /> Wins!</>)     
+        else if (gameState === STATUS.DRAW) setMessage(<>Draw!</>)
+    }, [gameState, turn])
 
-    function renderText() {
-        if (gameState === STATUS.WIN) {
-            return (
-                <>
-                    <Mark markType={turn} startSpin={true}></Mark> Wins!
-                </>
-            )
-        } else if (gameState === STATUS.DRAW) return <>Draw!</> 
-        else return <>Pause</>
-    }
-
-    function handleRestart(e) {
+    const handleRestart = useCallback(() => {
         console.debug("--- RESTART")
         toggleTurn()
-        toggleGameState(STATUS.INGAME)
-    }
+        toggleGameState(STATUS.RESTART);
+    }, [])
 
     return (
-        <>
-            <div className={`cover-page ${visible? null : "hidden" }`}>
-                <div className="tw-flex tw-flex-row tw-justify-center tw-items-center tw-text-8xl tw-gap-5"> 
-                    {renderText()}
-                </div>
-                <FloatButton text={"RESTART"} 
-                    onClick={handleRestart}>
-                </FloatButton>
-
-                <div className="tw-flex tw-flex-row tw-gap-5 tw-text-4xl">
-                    <a href="https://github.com/RedCarpG" target="_blank" rel="noreferrer">
-                        <FaGithubSquare />
-                    </a>
-                    <a href="https://www.linkedin.com/in/peng-gao-fr/" target="_blank" rel="noreferrer">
-                        <FaLinkedin />
-                    </a>
-                </div>
+        <CSSTransition in={gameState !== STATUS.INGAME} unmountOnExit timeout={700} classNames='end-page'>
+            <div className={`cover-page end-page`}>
+                <EndPageMessage text={message} />
+                <FloatButton text={"RESTART"} onClick={handleRestart} />
+                <EndPageIcons/>
             </div>
-        </>
+        </CSSTransition>
     );
 }
