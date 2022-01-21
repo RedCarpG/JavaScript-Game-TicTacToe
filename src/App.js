a818c706c2a6083a15592953faf2d6211fe66db9
RedCarpG <redcarpg@gmail.com>
2022-01-21 18:59:09 +0100

    update: App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import GameBoard from "./components/GameBoard";
+import GameContextProvider from "./components/GameContext";
+import EndPage from "./components/EndPage";
 
 function App() {
-  return ;
+
+  return (
+    <>
+
+        <div className="main-page tw-flex">
+          <div className="title"> Tic Tac Toe </div>
+          <GameContextProvider>
+            <GameBoard> </GameBoard>
+            <EndPage> </EndPage>
+          </GameContextProvider>
+        </div>
+
+    </>
+  );
 }
 
 export default App;
